6c235749383442314f955dfdac9d1ddbdb6c5590
snower <sujian199@gmail.com>
2014-12-29 15:03:34 +0800

    update

--- sevent/tcp.py
+++ sevent/tcp.py
@@ -1,41 +1,46 @@
 # -*- coding: utf-8 -*-
 
-import socket
-import event
-import loop as loop_
-from loop import instance
 import logging
-import collections
+from collections import deque
+import socket
 import errno
+import event
+from loop import instance, MODE_IN, MODE_OUT
 
-STATE_CLOSED = 0
-STATE_INITIALIZED = 1
-STATE_CONNECTING = 2
-STATE_STREAMING = 4
-STATE_LISTENING = 8
-STATE_CLOSING = 16
+
+STATE_INITIALIZED = 0x01
+STATE_CONNECTING = 0x02
+STATE_STREAMING = 0x04
+STATE_LISTENING = 0x08
+STATE_CLOSING = 0x10
+STATE_CLOSED = 0x20
 
 RECV_BUFSIZE = 4096
 
 class Socket(event.EventEmitter):
-    def __init__(self, loop=None, sock=None,addr=None):
+    def __init__(self, loop=None, socket=None, address=None):
         super(Socket, self).__init__()
-        self._socket = None
         self._loop =loop or instance()
-        self._addr=addr
-        self._buffers = collections.deque()
-        self._state = STATE_INITIALIZED
+        self._socket = socket
+        self._address = address
         self._connect_handler = None
+        self._read_handler = None
         self._write_handler = None
+        self._rbuffers = deque()
+        self._wbuffers = deque()
+        self._state = STATE_INITIALIZED
 
-        if sock is not None:
-            self._socket = sock
-            sock.setblocking(False)
-            self._init_streaming()
+        if self._socket:
+            self._state = STATE_STREAMING
+            self._read_handler = self._loop.add_fd(self._socket, MODE_IN, self._read_cb)
 
     @property
-    def addr(self):
-        return self._addr
+    def address(self):
+        return self._address
+
+    @property
+    def socket(self):
+        return self._socket
 
     def __del__(self):
         self.close()
@@ -45,13 +50,15 @@ class Socket(event.EventEmitter):
         if self._state in (STATE_INITIALIZED, STATE_CONNECTING):
             self.close()
         else:
-            if self._buffers:
+            if self._wbuffers:
                 self._state = STATE_CLOSING
             else:
                 self.close()
 
     def close(self):
-        if self._state == STATE_CLOSED:return
+        if self._state == STATE_CLOSED:
+            return
+
         if self._state == STATE_CONNECTING and self._connect_handler:
             self._loop.remove_handler(self._connect_handler)
             self._connect_handler = None
@@ -66,6 +73,7 @@ class Socket(event.EventEmitter):
                 self._socket.close()
             except Exception,e:
                 logging.error("socket close socket error:%s",e)
+
         self._state = STATE_CLOSED
         def on_close():
             self.emit('close', self)
@@ -78,30 +86,31 @@ class Socket(event.EventEmitter):
         logging.error("socket error:%s",error)
 
     def _connect_cb(self):
-        if self._state != STATE_CONNECTING:return
+        if self._state != STATE_CONNECTING:
+            return
         self._loop.remove_handler(self._connect_handler)
         self._connect_handler = None
-        self._init_streaming()
-        self._loop.sync(self.emit,'connect', self)
+
+        self._state = STATE_STREAMING
+        self._read_handler = self._loop.add_fd(self._socket, MODE_IN, self._read_cb)
+        self._loop.sync(self.emit, 'connect', self)
 
     def _timeout_cb(self):
-        if self._state==STATE_CONNECTING:
+        if self._state == STATE_CONNECTING:
             self._error(Exception("connect time out"))
 
-    def _init_streaming(self):
-        self._state = STATE_STREAMING
-        self._read_handler = self._loop.add_fd(self._socket, loop_.MODE_IN, self._read_cb)
-
-    def connect(self, address,timeout=5):
-        if self._state != STATE_INITIALIZED:return
+    def connect(self, address, timeout=5):
+        if self._state != STATE_INITIALIZED:
+            return
         try:
-            addrs = socket.getaddrinfo(address[0], address[1], 0, 0, socket.SOL_TCP)
+            addrinfo = socket.getaddrinfo(address[0], address[1], 0, 0, socket.SOL_TCP)
             # support both IPv4 and IPv6 addresses
-            if addrs:
-                addr = addrs[0]
-                self._socket = socket.socket(addr[0], addr[1], addr[2])
+            if addrinfo:
+                address = addrinfo[0]
+                self._socket = socket.socket(address[0], address[1], address[2])
                 self._socket.setblocking(False)
-                self._socket.connect(addr[4])
+                self._socket.connect(address[4])
+                self._address = address[4]
             else:
                 self._error(Exception('can not resolve hostname %s',address))
                 return
@@ -109,31 +118,32 @@ class Socket(event.EventEmitter):
             if e.args[0] not in (errno.EINPROGRESS, errno.EWOULDBLOCK):
                 self._error(e)
                 return
-        self._connect_handler = self._loop.add_fd(self._socket, loop_.MODE_OUT, self._connect_cb)
+        self._connect_handler = self._loop.add_fd(self._socket, MODE_OUT, self._connect_cb)
         self._loop.timeout(timeout,self._timeout_cb)
         self._state = STATE_CONNECTING
-        self._addr=addr[4]
 
     def _read_cb(self):
         if self._state in (STATE_STREAMING, STATE_CLOSING):
             self._read()
 
     def _read(self):
-        cache = []
         data = False
         while self._state in (STATE_STREAMING, STATE_CLOSING):
             try:
                 data = self._socket.recv(RECV_BUFSIZE)
-                if not data:break
-                cache.append(data)
+                if not data:
+                    break
+                self._rbuffers.append(data)
             except socket.error as e:
                 if e.args[0] in (errno.EWOULDBLOCK, errno.EAGAIN):break
                 else:
                     self._error(e)
                     return
 
-        if cache:
-            self._loop.sync(self.emit,'data', self, ''.join(cache))
+        if self._rbuffers:
+            self._loop.sync(self.emit,'data', self, ''.join(self._rbuffers))
+            self._rbuffers = deque()
+
         if not data:
             self._loop.sync(self.emit,'end', self)
             if self._state in (STATE_STREAMING, STATE_CLOSING):
@@ -141,21 +151,20 @@ class Socket(event.EventEmitter):
 
     def _write_cb(self):
         if self._state not in (STATE_STREAMING, STATE_CLOSING):return
-        result = self._write()
-        if result:
+        if self._write():
             self._loop.sync(self.emit,'drain', self)
 
     def _write(self):
-        while self._state in (STATE_STREAMING, STATE_CLOSING) and self._buffers:
-            data = self._buffers.popleft()
+        while self._state in (STATE_STREAMING, STATE_CLOSING) and self._wbuffers:
+            data = self._wbuffers.popleft()
             try:
                 r = self._socket.send(data)
                 if r < len(data):
-                    self._buffers.appendleft(data[r:])
+                    self._wbuffers.appendleft(data[r:])
                     return False
             except socket.error as e:
                 if e.args[0] in (errno.EWOULDBLOCK, errno.EAGAIN):
-                    self._buffers.appendleft(data)
+                    self._wbuffers.appendleft(data)
                 else:
                     self._error(e)
                 return False
@@ -170,50 +179,52 @@ class Socket(event.EventEmitter):
     def write(self, data):
         if self._state !=STATE_STREAMING:
             return False
-        self._buffers.append(data)
-        if not self._write() and not self._write_handler:
-            self._write_handler = self._loop.add_fd(self._socket, loop_.MODE_OUT, self._write_cb)
+        self._wbuffers.append(data)
+        if not self._write():
             if not self._write_handler:
-                self._error(Exception("write data add fd error"))
-                return False
+                self._write_handler = self._loop.add_fd(self._socket, MODE_OUT, self._write_cb)
+                if not self._write_handler:
+                    self._error(Exception("write data add fd error"))
+                    return False
             return True
         self._loop.sync(self.emit,'drain', self)
         return True
 
 
 class Server(event.EventEmitter):
-    def __init__(self, address, loop=None):
+    def __init__(self, loop=None):
         super(Server, self).__init__()
-        self._address = address
         self._loop = loop or instance()
         self._socket = None
-
-        addrs = socket.getaddrinfo(address[0], address[1], 0, 0, socket.SOL_TCP)
-        if addrs:
-            addr = addrs[0]
-            self._socket = socket.socket(addr[0], addr[1], addr[2])
-            self._socket.setblocking(False)
-            self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self._socket.bind(addr[4])
-        else:
-            self._error(Exception('can not resolve hostname %s' % address))
-
         self._state = STATE_INITIALIZED
 
     def __del__(self):
         self.close()
 
-    def listen(self, backlog=128):
-        if self._state != STATE_INITIALIZED:return
-        self._accept_handler = self._loop.add_fd(self._socket, loop_.MODE_IN, self._accept_cb)
-        self._socket.listen(backlog)
-        self._state = STATE_LISTENING
+    def listen(self, address, backlog=128):
+        if self._state != STATE_INITIALIZED:
+            return
+
+        addrinfo = socket.getaddrinfo(address[0], address[1], 0, 0, socket.SOL_TCP)
+        if addrinfo:
+            address = addrinfo[0]
+            self._socket = socket.socket(address[0], address[1], address[2])
+            self._socket.setblocking(False)
+            self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self._socket.bind(address[4])
+            self._accept_handler = self._loop.add_fd(self._socket, MODE_IN, self._accept_cb)
+            self._socket.listen(backlog)
+            self._state = STATE_LISTENING
+        else:
+            self._error(Exception('can not resolve hostname %s' % address))
 
     def _accept_cb(self):
-        if self._state != STATE_LISTENING:return
-        conn, addr = self._socket.accept()
-        sock = Socket(loop=self._loop, sock=conn,addr=addr)
-        self._loop.sync(self.emit,"connection",self,sock)
+        if self._state != STATE_LISTENING:
+            return
+
+        connection, address = self._socket.accept()
+        socket = Socket(loop=self._loop, socket=connection, address=address)
+        self._loop.sync(self.emit, "connection", self, socket)
 
     def _error(self, error):
         self._loop.sync(self.emit,'error', self, error)
--- sevent/udp.py
+++ sevent/udp.py
@@ -1,3 +1,130 @@
 # -*- coding: utf-8 -*-
 # 2014/12/28
 # create by: snower
+
+import logging
+import socket
+import errno
+from collections import deque
+from event import EventEmitter
+from loop import instance, MODE_IN, MODE_OUT
+
+STATE_STREAMING = 0x01
+STATE_BINDING = 0x02
+STATE_CLOSING = 0x04
+STATE_CLOSED = 0x06
+
+RECV_BUFSIZE = 4096
+
+class Socket(EventEmitter):
+    def __init__(self, loop=None):
+        super(Socket, self).__init__()
+        self._loop = loop or instance()
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.SOL_UDP)
+        self._socket.setblocking(False)
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._read_handler = self._loop.add_fd(self._socket, MODE_IN, self._read_cb)
+        self._write_handler = None
+        self._rbuffers = deque()
+        self._wbuffers = deque()
+        self._state = STATE_STREAMING
+
+    def __del__(self):
+        self.close()
+
+    def end(self):
+        if self._state in (STATE_STREAMING, STATE_BINDING):
+            if not self._wbuffers:
+                self.close()
+            else:
+                self._state = STATE_CLOSING
+
+    def close(self):
+        if self._state == STATE_CLOSED:
+            return
+        if self._read_handler:
+            self._loop.remove_handler(self._read_handler)
+        if self._write_handler:
+            self._loop.remove_handler(self._write_handler)
+        self._socket.close()
+        self._state = STATE_CLOSED
+        self._loop.sync(self.emit, 'close', self)
+        def on_close():
+            self.emit('close', self)
+            self.remove_all_listeners()
+        self._loop.sync(on_close)
+
+    def _error(self, error):
+        self._loop.sync(self.emit,'error', self, error)
+        logging.error("socket error:%s",error)
+        self.close()
+
+    def _read_cb(self):
+        if self._state in (STATE_STREAMING, STATE_BINDING):
+            self._read()
+
+    def _read(self):
+        while True:
+            try:
+                data, address = self._socket.recvfrom(RECV_BUFSIZE)
+                self._rbuffers.append(data)
+            except socket.error as e:
+                if e.args[0] in (errno.EWOULDBLOCK, errno.EAGAIN):break
+                else:
+                    return self._error(e)
+
+        if self._rbuffers:
+            self._loop.sync(self.emit, 'data', self, ''.join(self._rbuffers))
+            self._rbuffers = deque()
+
+    def _write_cb(self):
+        if self._state in (STATE_STREAMING, STATE_CLOSING, STATE_BINDING):
+            if self._write():
+                self._loop.sync(self.emit,'drain', self)
+
+    def _write(self):
+        while True:
+            address, data = self._wbuffers.popleft()
+            try:
+                r = self._socket.sendto(data, address)
+                if r < len(data):
+                    self._wbuffers.appendleft((address, data[r:]))
+                    return False
+            except socket.error as e:
+                if e.args[0] in (errno.EWOULDBLOCK, errno.EAGAIN):
+                    self._wbuffers.appendleft((address, data))
+                else:
+                    self._error(e)
+                return False
+
+        if self._write_handler:
+            self._loop.remove_handler(self._write_handler)
+            self._write_handler = None
+
+        if self._state == STATE_CLOSING:
+            self.close()
+        return True
+
+    def write(self, address, data):
+        if self._state not in (STATE_STREAMING, STATE_BINDING):
+            return False
+        self._wbuffers.append((address, data))
+        if not self._write() or self._write_handler is None:
+            self._write_handler = self._loop.add_fd(self._socket, MODE_OUT, self._write_cb)
+            if not self._write_handler:
+                self._error(Exception("write data add fd error"))
+                return False
+            return True
+        self._loop.sync(self.emit,'drain', self)
+        return True
+
+class Server(Socket):
+    def __init__(self, loop=None):
+        super(Server, self).__init__(loop)
+
+    def bind(self, address):
+        self._socket.bind(address)
+        self._state = STATE_BINDING
+
+    def __del__(self):
+        self.close()
