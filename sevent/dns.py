cb15cd01db5ce174560346ce9a21dc1127c1ad45
snower <sujian199@gmail.com>
2015-08-06 20:26:52 +0800

    fix

--- sevent/dns.py
+++ sevent/dns.py
@@ -190,7 +190,7 @@ class DNSResolver(EventEmitter):
     def send_req(self, hostname, qtype=None):
         qtype = ([QTYPE_A, QTYPE_AAAA] if qtype is None else [qtype]) if not isinstance(qtype, (list, tuple)) else qtype
         server_index = self._hostname_server_index.get(hostname, -1)
-        if(server_index + 1 >= len(self._servers)):
+        if server_index + 1 >= len(self._servers):
             self.call_callback(hostname, None)
         else:
             server = self._servers[server_index + 1]
