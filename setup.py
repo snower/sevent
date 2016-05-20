b17ae57c820df869d14d6afbccb924174bd8f4bf
snower <sujian199@gmail.com>
2016-05-20 13:40:50 +0800

    change version to 0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
     name = 'sevent',
-    version = '0.0.1',
+    version = '0.0.2',
     packages = ['sevent', 'sevent.impl'],
     package_data = {
         'sevent': ['README.md'],
--- sevent/__init__.py
+++ sevent/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-version = '0.0.1'
-version_info = (0,0,1)
+version = '0.0.2'
+version_info = (0,0,2)
 
 from loop import instance, current
 from event import EventEmitter
