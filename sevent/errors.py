88f27411aaec50b3050fc1dfa63b54fd5c27bee9
snower <sujian199@gmail.com>
2020-01-16 16:15:21 +0800

    add uncommit file

---
+++ sevent/errors.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# 2020/1/16
+# create by: snower
+
+class SeventException(Exception):
+    pass
+
+class SocketClosed(SeventException):
+    pass
+
+class ResolveError(SeventException):
+    pass
+
+class ConnectTimeout(SeventException):
+    pass
+
+class AddressError(SeventException):
+    pass
+
+class ConnectError(SeventException):
+    def __init__(self, address, socket_error, *args, **kwargs):
+        super(ConnectError, self).__init__(*args, **kwargs)
+
+        self.address = address
+        self.socket_error = socket_error
