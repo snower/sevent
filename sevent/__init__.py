c6918d5bd064a673b85d36b0efc8672fc6e8e700
snower <sujian199@gmail.com>
2020-04-19 15:45:53 +0800

    change version to 0.1.4

--- sevent/__init__.py
+++ sevent/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-version = '0.1.3'
-version_info = (0,1,3)
+version = '0.1.4'
+version_info = (0,1,4)
 
 from .loop import instance, current
 from .event import EventEmitter
