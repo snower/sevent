1c4de2cffbf820d6d3573ec6bf0bf55fe0bdb591
snower <sujian199@gmail.com>
2020-05-12 22:10:58 +0800

    change version 0.1.7

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
 
 setup(
     name='sevent',
-    version='0.1.6',
+    version='0.1.7',
     packages=['sevent', 'sevent.impl'],
     ext_modules=ext_modules,
     package_data={
--- sevent/__init__.py
+++ sevent/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-version = '0.1.6'
-version_info = (0, 1, 6)
+version = '0.1.7'
+version_info = (0, 1, 7)
 
 import sys
 
