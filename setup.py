4133a0b92fb716e0ac73ca8948704b140e4f3565
snower <sujian199@gmail.com>
2020-09-28 22:56:17 +0800

    change version 0.3.5

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
 
 setup(
     name='sevent',
-    version='0.3.4',
+    version='0.3.5',
     packages=['sevent', 'sevent.impl', 'sevent.coroutines', 'sevent.helpers'],
     ext_modules=ext_modules,
     package_data={
