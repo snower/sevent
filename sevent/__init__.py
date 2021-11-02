a11e4488e500a4008b07f4391119ad3288a1f07f
snower <sujian199@gmail.com>
2021-11-02 13:59:29 +0800

    change version to 0.4.6

--- sevent/__init__.py
+++ sevent/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-version = '0.4.5'
-version_info = (0, 4, 5)
+version = '0.4.6'
+version_info = (0, 4, 6)
 
 from .utils import is_py3, set_logger
 from .loop import instance, current
