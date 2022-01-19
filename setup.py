1e790d9df78ebdc248601cbf7c02bd96d3cd0db1
snower <sujian199@gmail.com>
2022-01-19 14:17:55 +0800

    change version to 0.4.7

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
 
 setup(
     name='sevent',
-    version='0.4.6',
+    version='0.4.7',
     packages=['sevent', 'sevent.impl', 'sevent.coroutines', 'sevent.helpers'],
     ext_modules=ext_modules,
     package_data={
--- sevent/__init__.py
+++ sevent/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-version = '0.4.6'
-version_info = (0, 4, 6)
+version = '0.4.7'
+version_info = (0, 4, 7)
 
 from .utils import is_py3, set_logger
 from .loop import instance, current
