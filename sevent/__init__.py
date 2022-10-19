96ec9ab33b942b6d732b69807512374ff8dbc10c
sujian <sujian@aecg.com.cn>
2022-10-19 16:59:38 +0800

    change version to 0.1.14

--- sevent/__init__.py
+++ sevent/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-version = '0.4.13'
-version_info = (0, 4, 13)
+version = '0.4.14'
+version_info = (0, 4, 14)
 
 from .utils import is_py3, set_logger
 from .loop import instance, current
