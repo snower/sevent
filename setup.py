171c8f0090571708cf852aedf45b0605a8edeb07
snower <sujian199@gmail.com>
2023-05-10 15:54:03 +0800

    change version to 0.4.16

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ else:
 
 setup(
     name='sevent',
-    version='0.4.15',
+    version='0.4.16',
     packages=['sevent', 'sevent.impl', 'sevent.coroutines', 'sevent.helpers'],
     ext_modules=ext_modules,
     package_data={
--- sevent/__init__.py
+++ sevent/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-version = '0.4.15'
-version_info = (0, 4, 15)
+version = '0.4.16'
+version_info = (0, 4, 16)
 
 from .utils import is_py3, set_logger
 from .loop import instance, current
