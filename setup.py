113c99158c1b95ad3f7629acb76900b087b4cafc
snower <sujian199@gmail.com>
2024-08-23 13:52:26 +0800

    Corrected the problem that the coroutine udp Server did not inherit the Socket and could not use the sendto and recvfrom functions

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ else:
 
 setup(
     name='sevent',
-    version='0.4.23',
+    version='0.4.24',
     packages=['sevent', 'sevent.impl', 'sevent.coroutines', 'sevent.helpers'],
     ext_modules=ext_modules,
     package_data={
