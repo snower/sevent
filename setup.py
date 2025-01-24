ae6136b3abc7a73320270a23c8003a878b33d1ca
sujian <sujian@aecg.com.cn>
2025-01-24 16:43:11 +0800

    change version to 0.4.26

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ else:
 
 setup(
     name='sevent',
-    version='0.4.25',
+    version='0.4.26',
     packages=['sevent', 'sevent.impl', 'sevent.coroutines', 'sevent.helpers'],
     ext_modules=ext_modules,
     package_data={
