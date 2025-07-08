a883415c820efd0dc402f3dd052847ff438bdb5f
sujian <sujian@aecg.com.cn>
2025-07-08 12:06:01 +0800

    Optimize Timing

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ else:
 
 setup(
     name='sevent',
-    version='0.4.36',
+    version='0.4.37',
     packages=['sevent', 'sevent.impl', 'sevent.coroutines', 'sevent.sslsocket', 'sevent.helpers'],
     ext_modules=ext_modules,
     package_data={
