df7f89e99a99e7b77ec3276a97bc0740848b62d3
sujian <sujian@aecg.com.cn>
2025-12-23 14:25:02 +0800

    change version 0.4.41

--- sevent/__init__.py
+++ sevent/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-version = '0.4.40'
-version_info = (0, 4, 40)
+version = '0.4.41'
+version_info = (0, 4, 41)
 
 from .utils import is_py3, set_logger
 from .loop import instance, current
